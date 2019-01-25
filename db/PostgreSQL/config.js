ced0bfe569f31c2fcab98ea43e533db4eeeab16f
Teddy An <teddy@Teddys-MacBook-Pro.local>
2019-01-24 16:57:28 -0800

    Changed config for deploying

--- db/PostgreSQL/config.js
+++ db/PostgreSQL/config.js
@@ -2,10 +2,11 @@ const { Client } = require('pg');
 
 // Creates a new Client with given information.
 const db = new Client ({
-  host: 'localhost',
-  user: 'teddy',
+  host: 'ec2-18-217-184-171.us-east-2.compute.amazonaws.com',
+  user: 'postgres',
   password: '',
   database: 'product_service',
+  port: '5432'
 });
 
 module.exports = db;
